5da56330bb0be5ced68c02aead81c500ec4f31a0
TeTeT Nimitz <tetet.nimitz@gmail.com>
2024-08-11 05:32:35 +0000

    Livery update by seabat

--- Liveries/vwv_a1_skyraider/A-1 VA-145 AC509/description.lua
+++ Liveries/vwv_a1_skyraider/A-1 VA-145 AC509/description.lua
@@ -4,9 +4,14 @@ livery = {
 	
 	{"18 - Default",	DIFFUSE				,	"A-1 NAVY TAILHOOK", false};
 	
-	{"fuel_150_a4e",	DIFFUSE			,	"A1 150 GAL USN", false};
+	{"fuel_150_a4e",	DIFFUSE			,	"A1 150 GAL USN VA-145", false};
 	{"AV8B7",	DIFFUSE			,	"A1 300 GAL USN", false};
-	{"vwv_A-1_propblur",    DIFFUSE                ,    "A-1 Skyraider PROP BLUR", false};
+	{"fuel_150_a4e",	ROUGHNESS_METALLIC	,	"A1 150 GAL ROUGHMET1", false};
+	
+	
+	
+	--{"vwv_A-1_propblur",	DIFFUSE				,	"A-1 Skyraider PROP BLUR", false};
+	--{"fuselage",		ROUGHNESS_METALLIC			,	"A_1_Seabat_RoughMet", false};
 	
 }
 name = "A-1 VA-145 AC509"
--- Liveries/vwv_a1_skyraider/A-1 VA-176 AC412/description.lua
+++ Liveries/vwv_a1_skyraider/A-1 VA-176 AC412/description.lua
@@ -6,7 +6,9 @@ livery = {
 	
 	{"fuel_150_a4e",	DIFFUSE			,	"A1 150 GAL USN", false};
 	{"AV8B7",	DIFFUSE			,	"A1 300 GAL USN", false};
-	{"vwv_A-1_propblur",    DIFFUSE                ,    "A-1 Skyraider PROP BLUR", false};
-
+	{"fuel_150_a4e",	ROUGHNESS_METALLIC	,	"A1 150 GAL ROUGHMET1", false};
+	
+	--{"vwv_A-1_propblur",	DIFFUSE				,	"A-1 Skyraider PROP BLUR", false};
+	--{"fuselage",		ROUGHNESS_METALLIC			,	"A_1_Seabat_RoughMet", false};
 }
 name = "A-1 VA-176 AC412"
